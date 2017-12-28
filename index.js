59fd2f8733b576578a7f1dcb6cee9b115ff6aa9e
Matthew Foyle <matt@moltin.com>
2017-12-28 23:33:27 +0000

    working pull of product data from airtable, parse and push to moltin

---
+++ index.js
@@ -0,0 +1,6 @@
+const stubs = require('./stubs/moltin-stubs.js');
+const moltinUtils = require('./utils/moltin');
+const airtableUtils = require('./utils/airtable');
+
+// call our airtable functionality
+airtableUtils.getAirtableRecords();
---
+++ stubs/moltin-stubs.js
@@ -0,0 +1,24 @@
+var exports = module.exports = {};
+
+exports.createProductStub = {
+    "type": "product",
+    "name": "My Great Product",
+    "slug": "my-great-product",
+    "sku": "MGP_001",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus lorem eget urna consequat, ut accumsan magna efficitur. Aliquam erat volutpat. Duis varius tempus elementum. Suspendisse pulvinar metus sed venenatis dapibus. Nulla quam metus, ultrices vitae ornare auctor, luctus vel tellus. Morbi nisl ligula, finibus gravida eros sed, congue consequat mi. Vivamus feugiat purus nulla, et venenatis ante porttitor quis. Pellentesque lobortis blandit semper. Proin id est sem.",
+    "manage_stock": true,
+    "price": [
+      {
+        "amount": 5891,
+        "currency": "USD",
+        "includes_tax": true
+      },
+      {
+        "amount": 7150,
+        "currency": "GBP",
+        "includes_tax": true
+      }
+    ],
+    "status": "live",
+    "commodity_type": "physical"
+}

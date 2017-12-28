bd027360ff4a668a14ac4215b8713214b86409c8
Matthew Foyle <matt@moltin.com>
2017-12-28 21:10:56 +0000

    setup airtable client and retrieve some records

---
+++ utils/airtable.js
@@ -0,0 +1,25 @@
+
+require('dotenv').config()
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE);
+
+base('Veggie Bin').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved ', record.id);
+        console.log(record.fields);
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});

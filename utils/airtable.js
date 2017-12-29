3cb340b99198b01eabc7f645429ad6f2d7abd0fc
Matthew Foyle <matt@moltin.com>
2017-12-29 14:01:35 +0000

    Re-indent and add try catches

--- utils/airtable.js
+++ utils/airtable.js
@@ -8,55 +8,58 @@ var Airtable = require('airtable');
 var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE);
 
 exports.getAirtableRecords = function(records) {
-    base('Veggie Bin').select({
+  base('Veggie Bin').select({
         // Selecting the first 3 records in Grid view:
         maxRecords: records,
         view: "Grid view"
-    }).eachPage(function page(records, fetchNextPage) {
+      }).eachPage(function page(records, fetchNextPage) {
         // This function (`page`) will get called for each page of records.
 
         records.forEach(function(record) {
 
-            console.log('Retrieved ' + record.id);
+          console.log('Retrieved ' + record.id);
 
-            if(record.fields.Status === 'Live') {
+          if(record.fields.Status === 'Live') {
 
-                setTimeout(function () {
+            setTimeout(function () {
 
-                    const processRecord = async (record) => {
-
-                        var createProduct = await moltin.createProduct({
-                        "type": "product",
-                        "name": record.fields.Name,
-                        "slug": record.fields['Slug 2'],
-                        "sku":  record.fields.sku,
-                        "description": record.fields.Descroption,
-                        "manage_stock": true,
-                        "price": [
-                        {
-                            "amount": record.fields.Price.trim().slice(1,8)*100,
-                            "currency": 'USD',
-                            "includes_tax": true
-                        }
-                        ],
-                        "status": "live",
-                        "stock": 5000,
-                        "commodity_type": record.fields['Commodity type'].toLowerCase()
-                    })
-
-                        var addRelationship = await moltin.addProductRelationship(createProduct.data.id, 'category', moltin.category(record.fields.Categories).id)
-                        
-                        var processFile = await moltin.fetchAndUploadFile(createProduct.data.id, record.fields.Name + ".jpg", record.fields.Image[0].url, './images/' + record.fields.Name + ".jpg")
+              const processRecord = async (record) => {
+                try {
+                  var createProduct = await moltin.createProduct({
+                    "type": "product",
+                    "name": record.fields.Name,
+                    "slug": record.fields['Slug 2'],
+                    "sku":  record.fields.sku,
+                    "description": record.fields.Descroption,
+                    "manage_stock": true,
+                    "price": [
+                    {
+                      "amount": record.fields.Price.trim().slice(1,8)*100,
+                      "currency": 'USD',
+                      "includes_tax": true
                     }
+                    ],
+                    "status": "live",
+                    "stock": 5000,
+                    "commodity_type": record.fields['Commodity type'].toLowerCase()
+                  })
 
-                    console.log('creating record ' + record.id);
+                  var addRelationship = await moltin.addProductRelationship(createProduct.data.id, 'category', moltin.category(record.fields.Categories).id)
+                  
+                  var processFile = await moltin.fetchAndUploadFile(createProduct.data.id, record.fields.Name + ".jpg", record.fields.Image[0].url, './images/' + record.fields.Name + ".jpg")
+                } catch(e) {
+                  console.log(e);
+                };
+              }
 
-                    processRecord(record);
-                    
-                }, 2000)
-            } else {
-                console.log('product is not live');
-            };
+              console.log('creating record ' + record.id);
+
+              processRecord(record);
+              
+            }, 2000)
+          } else {
+            console.log('product is not live');
+          };
         });
 
         // To fetch the next page of records, call `fetchNextPage`.
@@ -64,7 +67,7 @@ exports.getAirtableRecords = function(records) {
         // If there are no more records, `done` will get called.
         fetchNextPage();
 
-    }, function done(err) {
+      }, function done(err) {
         if (err) { console.error(err); return; }
-    });
-};
+      });
+    };
--- utils/moltin.js
+++ utils/moltin.js
@@ -13,28 +13,28 @@ const Moltin = MoltinGateway({
 });
 
 const categories = [
-  {'name': 'Bakery', 'id': '1686e358-40e3-4746-9bbc-ac9b21ab94ca'},
-  {'name': 'Beer and Wine', 'id': '548c782f-6b62-434a-ac8c-30cd999761e6'},
-  {'name': 'Beverages', 'id': '011ca5d5-765f-4e1a-bd04-9a3123326e79'},
-  {'name': 'Canned Goods', 'id': '648e582b-4f6a-4139-95b6-62d379e0cadd'},
-  {'name': 'Dairy & Eggs', 'id': '761abc79-cbee-4fba-97a4-df2324704dc5'},
-  {'name': 'Dry Goods & pasta', 'id': 'e7788bc2-646a-4626-bf19-cc80c048ae6d'},
-  {'name': 'Frozen', 'id': '2008300b-fe5c-4cf0-8f5d-912415c50dc1'},
-  {'name': 'Household', 'id': '19a9e630-f9a2-4ae5-ad52-cd234056488d'},
-  {'name': 'Meat & Seafood', 'id': '451fab4d-e421-43ce-b4ba-1ad05ad28e48'},
-  {'name': 'Pantry', 'id': '358d4a5b-1177-4677-9b03-fbadae1e91da'},
-  {'name': 'Produce', 'id': 'ae8b29bf-23f6-4121-a751-547c9e3be900'},
-  {'name': 'Snacks', 'id': 'ad95c01d-f2d7-4c3d-98df-1fc928d5935a'},
-  {'name': 'Pets', 'id': '2cf8a938-12b6-46a9-86bb-c805ba2119ab'},
-  {'name': 'Deli', 'id': 'a522cd06-a3da-4efe-9d8a-5a4434eb792c'},
-  {'name': 'Local', 'id': '1d95a879-95bd-45b1-9ca5-58f74d801a3e'}
+{'name': 'Bakery', 'id': '1686e358-40e3-4746-9bbc-ac9b21ab94ca'},
+{'name': 'Beer and Wine', 'id': '548c782f-6b62-434a-ac8c-30cd999761e6'},
+{'name': 'Beverages', 'id': '011ca5d5-765f-4e1a-bd04-9a3123326e79'},
+{'name': 'Canned Goods', 'id': '648e582b-4f6a-4139-95b6-62d379e0cadd'},
+{'name': 'Dairy & Eggs', 'id': '761abc79-cbee-4fba-97a4-df2324704dc5'},
+{'name': 'Dry Goods & pasta', 'id': 'e7788bc2-646a-4626-bf19-cc80c048ae6d'},
+{'name': 'Frozen', 'id': '2008300b-fe5c-4cf0-8f5d-912415c50dc1'},
+{'name': 'Household', 'id': '19a9e630-f9a2-4ae5-ad52-cd234056488d'},
+{'name': 'Meat & Seafood', 'id': '451fab4d-e421-43ce-b4ba-1ad05ad28e48'},
+{'name': 'Pantry', 'id': '358d4a5b-1177-4677-9b03-fbadae1e91da'},
+{'name': 'Produce', 'id': 'ae8b29bf-23f6-4121-a751-547c9e3be900'},
+{'name': 'Snacks', 'id': 'ad95c01d-f2d7-4c3d-98df-1fc928d5935a'},
+{'name': 'Pets', 'id': '2cf8a938-12b6-46a9-86bb-c805ba2119ab'},
+{'name': 'Deli', 'id': 'a522cd06-a3da-4efe-9d8a-5a4434eb792c'},
+{'name': 'Local', 'id': '1d95a879-95bd-45b1-9ca5-58f74d801a3e'}
 ];
 
 
 exports.category = function(name) {
 	return categories.find(category => {
-   	return category.name == name;
-	});
+    return category.name == name;
+  });
 }
 
 exports.addProductRelationship = (productID, type, ID) => {
@@ -51,10 +51,10 @@ exports.deleteProduct = (data) => {return Moltin.Products.Delete(data)};
 exports.deleteAllProducts = async () => {
 
   var getAllProducts = await exports.getAllProducts()
- 
-    getAllProducts.data.forEach(function(product) {
-      return exports.deleteProduct(product.id);
-    });
+  
+  getAllProducts.data.forEach(function(product) {
+    return exports.deleteProduct(product.id);
+  });
 };
 
 exports.manualAuth = () => {
@@ -63,22 +63,25 @@ exports.manualAuth = () => {
 
 exports.uploadFile = async (productID, name, path) => {
   
-  var auth = await exports.manualAuth()
+  try {
+    var auth = await exports.manualAuth()
 
-  const options = {
-    method: 'POST',
-    headers: {'Content-Type': 'multipart/form-data', 'Authorization': auth.access_token},
-    uri: 'https://api.moltin.com/v2/files',
-    formData: {
-      file: fs.createReadStream(path)
-    },
-    json: true
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'multipart/form-data', 'Authorization': auth.access_token},
+      uri: 'https://api.moltin.com/v2/files',
+      formData: {
+        file: fs.createReadStream(path)
+      },
+      json: true
+    };
+
+    var fetchFile = await rp(options)
+
+    var addRelationship = await exports.addProductRelationship(productID, 'file', fetchFile.data.id)
+  } catch(e) {
+    console.log(e);
   };
-
-  var fetchFile = await rp(options)
-
-  var addRelationship = await exports.addProductRelationship(productID, 'file', fetchFile.data.id)
-
 };
 
 exports.fetchAndUploadFile = async (productID, name, url, path) => {

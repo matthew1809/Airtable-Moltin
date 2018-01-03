932e98ddab7009fbb4eb116ce6efd5a65fba0a63
Matthew Foyle <matt@moltin.com>
2018-01-03 00:39:00 +0000

    Use async maplimit which operates "The same as map but runs a maximum of limit async operations at a time." - https://caolan.github.io/async/docs.html#mapLimit

--- utils/airtable.js
+++ utils/airtable.js
@@ -5,72 +5,69 @@ var https = require('https');
 var fs = require('fs');
 var moltin = require('./moltin');
 var Airtable = require('airtable');
-var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE);
+var async = require('async');
+var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_BURBAGES);
 
 exports.getAirtableRecords = function(records) {
-  base('Veggie Bin').select({
+  base('Burbages').select({
         // Selecting the first 3 records in Grid view:
         maxRecords: records,
         view: "Grid view"
       }).eachPage(function page(records, fetchNextPage) {
         // This function (`page`) will get called for each page of records.
 
-        records.forEach(function(record) {
-
-          console.log('Retrieved ' + record.id);
+        async.mapLimit(records, 2, async function(record) {
 
           if(record.fields.Status === 'Live') {
 
-            setTimeout(function () {
+            try {
 
-              const processRecord = async (record) => {
-                try {
+              let desc = "none";
 
-                  let desc = "none";
-
-                  if(record.fields.Descroption !== undefined) {
-                    desc = record.fields.Descroption;
-                    console.log(desc);
-                  }
-
-                  var createProduct = await moltin.createProduct({
-                    "type": "product",
-                    "name": record.fields.Name,
-                    "slug": record.fields['Slug 2'],
-                    "sku":  record.fields.sku,
-                    "description": desc,
-                    "manage_stock": true,
-                    "price": [
-                    {
-                      "amount": record.fields.Price.trim().slice(1,8)*100,
-                      "currency": 'USD',
-                      "includes_tax": true
-                    }
-                    ],
-                    "status": "live",
-                    "stock": 5000,
-                    "commodity_type": record.fields['Commodity type'].toLowerCase()
-                  });
-
-                  var catID = await moltin.category(record.fields.Categories).id;
-                  
-                  var addRelationship = await moltin.addProductRelationship(createProduct.data.id, 'category', catID)
-
-                  var processFile = await moltin.fetchAndUploadFile(createProduct.data.id, record.fields.Name + ".jpg", record.fields.Image[0].url, './images/' + record.fields.Name + ".jpg");
-
-                } catch(e) {
-                  console.log(e);
-                };
+              if(record.fields.Descroption !== undefined) {
+                desc = record.fields.Descroption;
+                console.log(desc);
               }
 
-              console.log('creating record ' + record.id);
+              var createProduct = await moltin.createProduct({
+                "type": "product",
+                "name": record.fields.Name,
+                "slug": record.fields['Slug 2'],
+                "sku":  record.fields.sku,
+                "description": desc,
+                "manage_stock": true,
+                "price": [
+                {
+                  "amount": record.fields.Price.trim().slice(1,8)*100,
+                  "currency": 'USD',
+                  "includes_tax": true
+                }
+                ],
+                "status": "live",
+                "stock": 5000,
+                "commodity_type": record.fields['Commodity type'].toLowerCase()
+              });
 
-              processRecord(record);
+              var catID = await moltin.category(record.fields.Categories).burbages_id;
 
-            }, 2000)
+              var addRelationship = await moltin.addProductRelationship(createProduct.data.id, 'category', catID)
+
+              var processFile = await moltin.fetchAndUploadFile(createProduct.data.id, record.fields.Name + ".jpg", record.fields.Image[0].url, './images/' + record.fields.Name + ".jpg");
+
+              return("done");
+
+            } catch(e) {
+              console.log(e);
+              return(e);
+            };
+
           } else {
             console.log('product is not live');
           };
+
+        }, (err, results) => {
+          if (err) throw err
+            console.log(results);
         });
 
         // To fetch the next page of records, call `fetchNextPage`.

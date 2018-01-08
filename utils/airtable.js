04d5645940a8139417a4bff8c3c0064b3d118a03
Matthew Foyle <matt@moltin.com>
2018-01-08 14:47:05 +0000

    - Process records one by one

--- utils/airtable.js
+++ utils/airtable.js
@@ -1,12 +1,11 @@
-var exports = module.exports = {};
-
 require('dotenv').config()
-var https = require('https');
-var fs = require('fs');
-var moltin = require('./moltin');
-var Airtable = require('airtable');
-var async = require('async');
-var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_BURBAGES);
+const exports = module.exports = {};
+const https = require('https');
+const fs = require('fs');
+const moltin = require('./moltin');
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_BURBAGES);
+const timeout = ms => new Promise(res => setTimeout(res, ms))
 
 exports.getAirtableRecords = function(records) {
   base('Burbages').select({
@@ -16,59 +15,84 @@ exports.getAirtableRecords = function(records) {
       }).eachPage(function page(records, fetchNextPage) {
         // This function (`page`) will get called for each page of records.
 
-        async.mapLimit(records, 2, async function(record) {
+        async function moltinProcess(record, desc) {
 
-          if(record.fields.Status === 'Live') {
+          try {
 
-            try {
-
-              let desc = "none";
-
-              if(record.fields.Descroption !== undefined) {
-                desc = record.fields.Descroption;
-                console.log(desc);
+            var createProduct = await moltin.createProduct({
+              "type": "product",
+              "name": record.fields.Name,
+              "slug": record.fields['Slug 2'],
+              "sku":  record.fields.sku,
+              "description": desc,
+              "manage_stock": true,
+              "price": [
+              {
+                "amount": record.fields.Price.trim().slice(1,8)*100,
+                "currency": 'USD',
+                "includes_tax": true
               }
+              ],
+              "status": "live",
+              "stock": 5000,
+              "commodity_type": record.fields['Commodity type'].toLowerCase()
+            });
 
-              var createProduct = await moltin.createProduct({
-                "type": "product",
-                "name": record.fields.Name,
-                "slug": record.fields['Slug 2'],
-                "sku":  record.fields.sku,
-                "description": desc,
-                "manage_stock": true,
-                "price": [
-                {
-                  "amount": record.fields.Price.trim().slice(1,8)*100,
-                  "currency": 'USD',
-                  "includes_tax": true
-                }
-                ],
-                "status": "live",
-                "stock": 5000,
-                "commodity_type": record.fields['Commodity type'].toLowerCase()
-              });
+            var catID = await moltin.category(record.fields.Categories).burbages_id;
 
-              var catID = await moltin.category(record.fields.Categories).burbages_id;
+            var addRelationship = await moltin.addProductRelationship(createProduct.data.id, 'category', catID)
 
-              var addRelationship = await moltin.addProductRelationship(createProduct.data.id, 'category', catID)
+            var processFile = await moltin.fetchAndUploadFile(createProduct.data.id, record.fields.Name + ".jpg", record.fields.Image[0].url, './images/' + record.fields.Name + ".jpg");
 
-              var processFile = await moltin.fetchAndUploadFile(createProduct.data.id, record.fields.Name + ".jpg", record.fields.Image[0].url, './images/' + record.fields.Name + ".jpg");
+            return("done");
 
-              return("done");
+          } catch(e) {
+            return e;
+          }
 
-            } catch(e) {
-              console.log(e);
-              return(e);
-            };
+        };
 
-          } else {
-            console.log('product is not live');
-          };
 
-        }, (err, results) => {
-          if (err) throw err
-            console.log(results);
-        });
+        async function processThing(record) {
+
+         if(record.fields.Status === 'Live') {
+
+          let desc = "none";
+
+          if(record.fields.Descroption !== undefined) {
+            desc = record.fields.Descroption;
+            console.log(desc);
+          }
+
+          var process = await moltinProcess(record, desc);
+          var timer = await timeout(2000);
+          console.log(process);
+          return;
+
+        } else {
+          console.log('product is not live');
+        };
+
+      };
+
+      function processAllThings(things, cb) {
+
+        var currentThing = things.shift();
+
+        processThing(currentThing)
+        .then(() => {
+
+          if (things.length > 0) {
+            processAllThings(things)
+          }  
+
+        }).catch((e) => {
+          console.log(e);
+        })
+      }
+
+      processAllThings(records);
+
 
         // To fetch the next page of records, call `fetchNextPage`.
         // If there are more records, `page` will get called again.
